0691db3a512b243d0ce009deb96c11c432450209
miquelburguera <miquelburguera@alumnat.iesdamiahuguet.net>
2026-02-13 11:41:13 +0100

    color aleatori

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+const colors = ["#005f73", "#0a9396", "#94d2bd", "#e9d8a6", "#ee9b00", "#ca6702", "#bb3e03", "#ae2012", "#9b2226"];
+const color = [];
+
 /*
     Mostra la notificació al panell notification
     Mostra el color hexadecimal passat per paràmetre
  */
+
+function inici() {
+    numAleatori = Math.floor(Math.random() * colors.length);
+    showNotification(colors[numAleatori]);
+}
+
 
 function showNotification(message) {
     console.log(message)
