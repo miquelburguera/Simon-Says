aca54c8f1b19b29d3f4677763a4ef48bab410986
miquelburguera <miquelburguera@alumnat.iesdamiahuguet.net>
2026-03-03 11:04:59 +0100

    Final

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const colors = ["#005f73", "#0a9396", "#94d2bd", "#e9d8a6", "#ee9b00", "#ca6702", "#bb3e03", "rgba(174, 32, 18, 0.89)", "#9b2226"];
 const sequencia = [];
 let comptadorResultats;
+let gameover =false;
+
 
 const missatgeDisplay = document.getElementById("message");
 /*
@@ -8,8 +10,12 @@ const missatgeDisplay = document.getElementById("message");
     Mostra el color hexadecimal passat per paràmetre
  */
 //triar els colors
+
 function inici(){
+    sequencia.splice(0,sequencia.length);
+    document.getElementById("botoReiniciar").hidden = true;
     document.getElementById("botoComencar").hidden = true;
+    gameover=false;
     nouColor()
 }
 
@@ -47,19 +53,20 @@ function showNotification() {
 
 function comprovarColor(color) {
     console.log(color)
-
-
+    if (!gameover) {
         if (color === sequencia[comptadorResultats]) {
             comptadorResultats += 1
             if (comptadorResultats === sequencia.length) {
                 nouColor()
             }
         } else {
+            gameover = true;
             console.log('no')
             missatgeDisplay.innerHTML = '<h3>GAME OVER!!!</h3>' + '<h2>Has arribat al nivell ' + sequencia.length + '</h2>'
-
+            document.getElementById("botoReiniciar").hidden = false;
         }
+    }
 
 }
 
-
+document.getElementById("botoReiniciar").hidden = true;
